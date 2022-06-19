162b3c42961ac9091e29988f0e06c2d53b99e592
xeedis <k.tarnowski99@gmail.com>
2022-06-19 19:16:41 +0200

    photo-upload-backend

--- GymSharkApi/Controllers/ProductsController.cs
+++ GymSharkApi/Controllers/ProductsController.cs
@@ -2,7 +2,9 @@
 using GymSharkApi.DTOs;
 using GymSharkApi.Entities;
 using GymSharkApi.Interfaces;
+using GymSharkApi.Services;
 using GymSharkAPI.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -16,11 +18,13 @@ namespace GymSharkApi.Controllers
         private readonly DataContext _context;
         private readonly IProductRepository _productRepository;
         private readonly IMapper _mapper;
-        public ProductsController(DataContext context, IProductRepository productRepository, IMapper mapper)
+        private readonly IPhotoService _photoService;
+        public ProductsController(DataContext context, IProductRepository productRepository, IMapper mapper, IPhotoService photoService)
         {
             _context = context;
             _productRepository = productRepository;
             _mapper = mapper;
+            _photoService = photoService;
         }
 
         [HttpGet]
@@ -47,6 +51,35 @@ namespace GymSharkApi.Controllers
 
             if (await _productRepository.SaveAllAsync()) return NoContent();
             return BadRequest("Failed to update item!");
+        }
+
+        [HttpPost("{productName}/add-photo", Name ="GetProduct")]
+        public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file, string productName)
+        {
+            var product = await _productRepository.GetProductByName(productName);
+
+            var result = await _photoService.AddPhotoAsync(file);
+
+            if (result.Error != null) return BadRequest(result.Error.Message);
+
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId
+            };
+
+            if(product.Photos.Count == 0)
+            {
+                photo.isMain = true;
+            }
+
+            product.Photos.Add(photo);
+
+            if(await _productRepository.SaveAllAsync())
+            {
+                return CreatedAtRoute("GetProduct",new { productName = product.ProductName},_mapper.Map<PhotoDto>(photo));
+            }
+            return BadRequest("Problem with adding photo!");
         }
     }
 }
